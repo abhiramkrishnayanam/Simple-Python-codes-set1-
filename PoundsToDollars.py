ce12be6e92b2141c22c9743c3786ebeda87e7642
Abhiram K <abhiramkrishnayanam@gmail.com>
2024-10-05 12:41:57 +0530

    Add files via upload

---
+++ PoundsToDollars.py
@@ -0,0 +1,11 @@
+# Exercise 10 Name your file: PoundsToDollars.py Write a program that asks the user to
+# enter an amount in pounds (£) and the program calculates and converts an amount in dollar ($).
+# amount=float(input("enter your amount in pound "))
+# exchange_rate=1.23
+# if amount==0:
+#     amount=float(input(" please give an other than zero :"))
+#     dollar = amount * exchange_rate
+#     print(f"The amount in dollar is {dollar}")
+# else:
+#     dollar = amount*exchange_rate
+#     print(f"The amount in dollar is {dollar}")
---
+++ Python Assignment 1.py
@@ -0,0 +1,75 @@
+# Exercise 1 Write Python code that prints your name, student number and email address.
+# An example runs of the program: Bob ST1001 bob@gmail.com
+# name = str(input("enter your name "))
+# email = str(input("enter your mail id "))
+# stdnum = str(input("enter your student number in the course "))
+# print (f"Your name is {name} ")
+# print (f"Your email id is {email} ")
+# print (f"Your Student number is {stdnum} ")
+
+# Exercise 2 Write Python code that prints your name, student number and email address using escape sequences.
+# name = str(input("enter your name "))
+# email = str(input("enter your mail id "))
+# stdnum = str(input("enter your student number in the course "))
+# #used \n as escape sequence
+# print (f"Your name is {name}\nYour email id is {email}\nYour Student number is {stdnum}")
+
+# Exercise 3 Write Python code that add, subtract, multiply and divide the two numbers.
+# You can use the two numbers 14 and 7.
+# x=14
+# y=7
+# print(f"sum of the numbers is {x+y}\ndifference of the numbers is {x-y}"
+#       f"\nproduct of the numbers is {x*y}\nquotient when x is divided by y is {int(x/y)}")
+
+# Exercise 4 Write Python code that displays the numbers from 1 to 5 as steps.
+# for i in range (1,6):
+#     print(i)
+
+# Exercise 5 Write Python code that outputs the following sentence (including the quotation
+# marks and line break) to the screen: An example runs of the program: "SDK" stands for
+# "Software Development Kit", whereas "IDE" stands for "Integrated Development Environment".
+# print("\"SDK\" stands for \"Software Development Kit\","
+#       "\nwhereas \"IDE\" stands for \"Integrated Development Environment\". ")
+
+# Exercise 6 Practice and check the output
+# print("python is an \"awesome\" language.")
+# print("python\n\t2023")
+# print('I\'m from Entri.\b')
+# print("\65")
+# print("\x65")
+# print("\"Entri\", \"2023\", sep=\n")
+# print("\"Entri\", \"2023\", sep=\b")
+# print("\"Entri\", \"2023\", sep=\"*\", end=\b\b\b\b")
+
+# Exercise 7 Define the variables below. Print the types of each variable.
+# What is the sum of your variables? (Hint: use a type conversion function.)
+# What datatype is the sum? num=23 textnum="57" decimal=98.3
+# num=23
+# textnum="57"
+# decimal=98.3
+# print(f"data type of num is {type(num)}\ndata type of textnum is {type(textnum)}"
+#       f"\ndata type of decimal is {type(decimal)}")
+# sum=float(23+float(textnum)+decimal)
+# print (f"the sum of num,textnum and decimal is {sum} with datatype {type(sum)}. ")
+
+# Exercise 8 calculate the number of minutes in a year using variables for each unit of time.
+# print a statement that describes what your code does also.
+# Create three variables to store no of days in a year, minute in a hour, hours in a day,
+# then calculate the total minutes in a year and print the values (hint) total number of
+# minutes in an year =No.of days in an year * Hours in a day * Minutes in an hour.
+
+# min_in_hour = 60#number of minutes in an hour
+# hours_in_day =24
+# days_in_year=365
+# mins_in_year = days_in_year * hours_in_day * min_in_hour
+# print("Here in a hour there are 60 minutes and in a day 24 hours ,then in an year 365 days so")
+# print(f"the total number of minutes in an year is {mins_in_year}.")
+
+# Exercise 9 Write Python code that asks the user to enter his/her name and then
+# output/prints his/her name with a greeting.
+# An example runs of the program: Please enter you name: Tony Hi Tony, welcome to Python programming :)
+# name=input("enter your name ")
+# print(f"Hi {name},\n Welcome to the world of programing. ")
+
+
+
